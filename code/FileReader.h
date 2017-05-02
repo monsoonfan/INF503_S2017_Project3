a7bbd3db0774c72fff2f71452449a2b943482eeb
JMistral <jc3464@nau.edu>
2017-05-02 13:36:33 -0700

    temp

--- code/FileReader.h
+++ code/FileReader.h
@@ -5,9 +5,9 @@ class FileReader
 public:
 	FileReader();
 	~FileReader();
-	int nextC(FILE* fa);
-	int PreProcessSubjects(char*, char*, int);
-	int PreProcessQueries(char*, char*, int);
+	//int nextC(FILE* fa);
+	//int PreProcessSubjects(char*, char*, int);
+	//int PreProcessQueries(char*, char*, int);
 	int ReadSubjects(char *, char *, int);
 	int ReadQueries(char *, char *, int);
 };

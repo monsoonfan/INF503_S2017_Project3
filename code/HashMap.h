bff52ba2dbb3d54172b918d4640f352a5a5a621e
Jiaming <jc3464@nau.edu>
2017-05-01 21:41:18 -0700

    hash of hashes 1

--- code/HashMap.h
+++ code/HashMap.h
@@ -14,10 +14,26 @@
 
 unsigned int MurmurHash2 ( const void * , int, unsigned int);
 
+struct node {
+	int location;
+	node* next;
+};
+
+class SeedEntry {
+	private:
+		unsigned int value; // this is the hashed value of this seed
+		node * head; // the list of locations where this seed present
+		
+	public:
+		SeedEntry(int); // input: the size of this location table
+		void retrieve(node &*);
+		
+};
+
 class HashEntry {
 	private:
 	 	int taxa;
-	 	unsigned int *value;
+	 	SeedEntry *locations;
 	 	unsigned int size;
 	 	unsigned int cursor;
 	 	
@@ -37,10 +53,12 @@ class HashMap{
 	public:
 		HashMap();
 		int get(int,int);
-		void addTax(int, unsigned int);
-		void addSeed(int, const void *);
+		void addTax(int, unsigned int); // input: taxaID, size of this taxID
+		void addSeed(int, const void *,int); // input taxID, seed string, position of this seed
 		void Initialize(char *);
 		~HashMap();
 };
+
+
 #endif
 

71872e85bbd74dc5310cd0dc1087d8e326a05838
Ryan Ralston ~ student <rmr5@wind.nauhpc>
2017-05-03 23:13:18 -0700

    increase mem limit and fix cmd invocation

--- misc_scripts/test_virus_reads.sh
+++ misc_scripts/test_virus_reads.sh
@@ -3,12 +3,12 @@
 #SBATCH --output=/scratch/rmr5/project/test_virus_reads.log
 #SBATCH --time=1:00:00
 #SBATCH --workdir=/scratch/rmr5/project
-#SBATCH --mem=2000
+#SBATCH --mem=40000
 
 # load a module, for example
 module load gcc/6.2.0
 
 # HW1 P2.c
 date
-./Project3 /common/contrib/classroom/inf503/project_3/small_test/viral_seqs.fasta /common/contrib/classroom/inf503/project_3/small_test/test_reads.fasta 5000000 test.out
+/home/rmr5/INF503/project/INF503_S2017_Project3/code/Project3 /common/contrib/classroom/inf503/project_3/small_test/viral_seqs.fasta /common/contrib/classroom/inf503/project_3/small_test/test_reads.fasta 100000000 test.out /home/rmr5/INF503/project/INF503_S2017_Project3/data/TableSizeViruses.txt
 date
